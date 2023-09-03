183e440c640d21d09e2c3eba4c4c698ea8d6e4ca
Aleksandr <deadone.2018@inbox.ru>
2023-09-04 00:15:01 +0400

    Задача со степенями

---
+++ stepen/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double stepen (int A, int B) {
+    double resualt = Math.Pow(A,B);
+    Console.WriteLine(resualt);
+    return resualt;
+}
+
+
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+
+stepen(A,B);
